ebd9325a45d436822787360e108f1c8ac1863a1c
ChrGri <christophergrimm88@web.de>
2024-01-26 18:41:06 +0100

    Working version of forcetargetingstrategy

--- Arduino/Esp32/Main/StepperMovementStrategy.h
+++ Arduino/Esp32/Main/StepperMovementStrategy.h
@@ -219,13 +219,13 @@ int32_t MoveByForceTargetingStrategy(float loadCellReadingKg, StepperWithLimits*
     // this can be tuned for responsiveness vs oscillation
     float mm_per_motor_rev = 10;//TRAVEL_PER_ROTATION_IN_MM;
     float steps_per_motor_rev = STEPS_PER_MOTOR_REVOLUTION;
-    float move_mm_per_kg = config_st->payLoadPedalConfig_.PID_p_gain;
+    float move_mm_per_kg = 5.0 * config_st->payLoadPedalConfig_.PID_p_gain;
     float MOVE_STEPS_FOR_1KG = (move_mm_per_kg / mm_per_motor_rev) * steps_per_motor_rev;
     
     float m1 = -1000;
     if (MOVE_STEPS_FOR_1KG > 0)
     {
-      float m1 = -1. / MOVE_STEPS_FOR_1KG; // line has negative slope --> pedal moves towards the front endstop will increase the loadcell reading as it pushes more against the foot
+      m1 = -1. / MOVE_STEPS_FOR_1KG; // line has negative slope --> pedal moves towards the front endstop will increase the loadcell reading as it pushes more against the foot
     }
     
     float m2 = gradient_force_curve_fl32;
@@ -251,7 +251,7 @@ int32_t MoveByForceTargetingStrategy(float loadCellReadingKg, StepperWithLimits*
     
     
     
-  if (1)
+  if (0)
   {
     Serial.print(b1);
     Serial.print(", ");
