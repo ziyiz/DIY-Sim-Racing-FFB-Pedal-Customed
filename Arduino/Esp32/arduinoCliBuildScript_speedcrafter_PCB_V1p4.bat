0e12f2aa3d84708a5bd74837751ba40290d36661
ChrGri <christophergrimm88@web.de>
2024-02-17 18:13:13 +0100

    Added missing binary for speedcrafters pcb

---
+++ Arduino/Esp32/arduinoCliBuildScript_speedcrafter_PCB_V1p4.bat
@@ -0,0 +1,2 @@
+arduino-cli compile --fqbn esp32:esp32:esp32wrover --output-dir ./Arduino/Esp32/bin_speedcrafter_PCB_V1p4 --libraries ./Arduino/libs/ --build-property build.extra_flags=-DPCB_VERSION=5 ./Arduino/Esp32/Main
+
