b405a8a8ab2bb9ff3f6675b0fa0ee3acbaca27c0
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2024-10-31 13:00:36 +0800

    update the bat file

---
+++ OTA/Build_control_board.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Save the current directory
+set CUR_DIR=%cd%
+
+REM Change to the directory where the PlatformIO project is located (relative to this batch file)
+cd /d "%~dp0\..\ESP32\"
+
+REM Run the PlatformIO build command
+platformio run
+
+REM Change back to the original directory
+cd /d %CUR_DIR%
+
+REM Pause to keep the command prompt open after the build
+pause
