6426ee941b26d2d63e6b183198f56a2a6472e53c
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2024-11-02 16:30:41 +0800

    DevBuild-servo reliability and OTA update improvement
    1. Improve servo reliability and response
    2. Add Fanatec-bridge firmware to build(Contribute by Oro)
    3. OTA now work with pull mode(Only fro ESP32S3 user, which including V4 and Gilphilbert PCBA, ESP32 user still can use old upload method)
    4. UI improveement

--- OTA/copy_to_OTA_folder_ControlBoard.bat
+++ OTA/copy_to_OTA_folder_ControlBoard.bat
@@ -5,7 +5,31 @@ set destination=..\OTA\ControlBoard\esp32\
 echo Copying %source% to %destination%...
 xcopy "%source%" "%destination%" /y
 
+set source=..\ESP32\.pio\build\esp32\bootloader.bin
+set destination=..\OTA\ControlBoard\esp32\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32\partitions.bin
+set destination=..\OTA\ControlBoard\esp32\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
 set source=..\ESP32\.pio\build\esp32s3usbotg\firmware.bin
+set destination=..\OTA\ControlBoard\esp32S3\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32s3usbotg\bootloader.bin
+set destination=..\OTA\ControlBoard\esp32S3\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32s3usbotg\partitions.bin
 set destination=..\OTA\ControlBoard\esp32S3\
 
 echo Copying %source% to %destination%...
@@ -17,7 +41,31 @@ set destination=..\OTA\ControlBoard\Gilphilbert_1_2\
 echo Copying %source% to %destination%...
 xcopy "%source%" "%destination%" /y
 
+set source=..\ESP32\.pio\build\esp32s3usbotg-gilphilbert\bootloader.bin
+set destination=..\OTA\ControlBoard\Gilphilbert_1_2\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32s3usbotg-gilphilbert\partitions.bin
+set destination=..\OTA\ControlBoard\Gilphilbert_1_2\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
 set source=..\ESP32\.pio\build\esp32s3usbotg-gilphilbert_2_0\firmware.bin
+set destination=..\OTA\ControlBoard\Gilphilbert_2_0\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32s3usbotg-gilphilbert_2_0\bootloader.bin
+set destination=..\OTA\ControlBoard\Gilphilbert_2_0\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32s3usbotg-gilphilbert_2_0\partitions.bin
 set destination=..\OTA\ControlBoard\Gilphilbert_2_0\
 
 echo Copying %source% to %destination%...
@@ -29,6 +77,17 @@ set destination=..\OTA\ControlBoard\Speedcrafter\
 echo Copying %source% to %destination%...
 xcopy "%source%" "%destination%" /y
 
+set source=..\ESP32\.pio\build\esp32-speedcrafter\bootloader.bin
+set destination=..\OTA\ControlBoard\Speedcrafter\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
+
+set source=..\ESP32\.pio\build\esp32-speedcrafter\partitions.bin
+set destination=..\OTA\ControlBoard\Speedcrafter\
+
+echo Copying %source% to %destination%...
+xcopy "%source%" "%destination%" /y
 
 echo File copied successfully.
 pause
